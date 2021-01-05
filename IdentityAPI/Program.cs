a8083c544f7df1e4bbdf5a24b29271bc6537a40a
UnknownUser <harvardaidan3@hotai>
2021-01-04 20:51:49 -0800

    Run as admin

--- IdentityAPI/Program.cs
+++ IdentityAPI/Program.cs
@@ -14,20 +14,20 @@ namespace IdentityAPI
     {
         public static void Main(string[] args)
         {
-            //BuildWebHost(args).Run();
-            var host = new WebHostBuilder()
+            BuildWebHost(args).Run();
+            /*var host = new WebHostBuilder()
               .UseKestrel()
               .UseContentRoot(Directory.GetCurrentDirectory())
               .UseIISIntegration()
               .UseStartup<Startup>()
               .Build();
 
-            host.Run();
+            host.Run();*/
 
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost.CreateDefaultBuilder(args).UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
     }
